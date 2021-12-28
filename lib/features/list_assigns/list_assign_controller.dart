67118960f78436c65db5f5cb4561001194b9c6a7
Ulises <amao.apps@gmail.com>
2021-12-28 10:46:44 -0500

    added list assigns

--- lib/features/list_assigns/list_assign_controller.dart
+++ lib/features/list_assigns/list_assign_controller.dart
@@ -1,7 +1,26 @@
+import 'package:clean_app/data/model/child.dart';
+import 'package:clean_app/data/repository/child_repository.dart';
 import 'package:get/get.dart';
 
 class ListAssignController extends GetxController {
 
   var isLoading = false;
+  var listChildren = List<Child>.empty().obs;
+
+  @override
+  void onInit(){
+    super.onInit();
+    getChildren();
+  }
+
+   
+  Future<List<Child>?> getChildren() async {
+    ChildRepository repo = ChildRepositoryImpl();
+    var list = await repo.getListChild();
+    if(list!=null){
+      listChildren.value = list;
+    }
+    return await repo.getListChild();
+  }
 
 }
